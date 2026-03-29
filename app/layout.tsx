1c6567bded73a8616dcf250e90a2fdf597010086
WhoamiI00 <ankitisop12@gmail.com>
2026-03-29 11:09:38 +0530

    fonts fice

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,3 @@
-import {
-  Work_Sans,
-  Inter,
-  Roboto,
-  Open_Sans,
-  Lato,
-  Montserrat,
-  Poppins,
-  Playfair_Display,
-  Merriweather,
-  Lora,
-  Fira_Code,
-  Source_Code_Pro,
-  JetBrains_Mono,
-  Pacifico,
-  Dancing_Script,
-} from "next/font/google";
-
 import "./globals.css";
 import { TooltipProvider } from "@/components/ui/tooltip";
 
@@ -25,94 +7,37 @@ export const metadata = {
     "NeoLive: Real-time collaborative design platform with advanced editing tools, real-time collaboration, and professional design features",
 };
 
-const workSans = Work_Sans({
-  subsets: ["latin"],
-  variable: "--font-work-sans",
-  weight: ["400", "600", "700"],
-});
-
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
-});
-
-const roboto = Roboto({
-  subsets: ["latin"],
-  variable: "--font-roboto",
-  weight: ["300", "400", "500", "700"],
-});
-
-const openSans = Open_Sans({
-  subsets: ["latin"],
-  variable: "--font-open-sans",
-});
-
-const lato = Lato({
-  subsets: ["latin"],
-  variable: "--font-lato",
-  weight: ["300", "400", "700"],
-});
-
-const montserrat = Montserrat({
-  subsets: ["latin"],
-  variable: "--font-montserrat",
-});
-
-const poppins = Poppins({
-  subsets: ["latin"],
-  variable: "--font-poppins",
-  weight: ["300", "400", "500", "600", "700"],
-});
-
-const playfairDisplay = Playfair_Display({
-  subsets: ["latin"],
-  variable: "--font-playfair-display",
-});
-
-const merriweather = Merriweather({
-  subsets: ["latin"],
-  variable: "--font-merriweather",
-  weight: ["300", "400", "700"],
-});
-
-const lora = Lora({
-  subsets: ["latin"],
-  variable: "--font-lora",
-});
-
-const firaCode = Fira_Code({
-  subsets: ["latin"],
-  variable: "--font-fira-code",
-});
-
-const sourceCodePro = Source_Code_Pro({
-  subsets: ["latin"],
-  variable: "--font-source-code-pro",
-});
-
-const jetbrainsMono = JetBrains_Mono({
-  subsets: ["latin"],
-  variable: "--font-jetbrains-mono",
-});
-
-const pacifico = Pacifico({
-  subsets: ["latin"],
-  variable: "--font-pacifico",
-  weight: ["400"],
-});
-
-const dancingScript = Dancing_Script({
-  subsets: ["latin"],
-  variable: "--font-dancing-script",
-});
-
-const fontVariables = `${workSans.variable} ${inter.variable} ${roboto.variable} ${openSans.variable} ${lato.variable} ${montserrat.variable} ${poppins.variable} ${playfairDisplay.variable} ${merriweather.variable} ${lora.variable} ${firaCode.variable} ${sourceCodePro.variable} ${jetbrainsMono.variable} ${pacifico.variable} ${dancingScript.variable}`;
+const GOOGLE_FONTS_URL =
+  "https://fonts.googleapis.com/css2?" +
+  "family=Work+Sans:wght@400;600;700" +
+  "&family=Inter:wght@300;400;500;600;700" +
+  "&family=Roboto:wght@300;400;500;700" +
+  "&family=Open+Sans:wght@300;400;500;600;700" +
+  "&family=Lato:wght@300;400;700" +
+  "&family=Montserrat:wght@300;400;500;600;700" +
+  "&family=Poppins:wght@300;400;500;600;700" +
+  "&family=Playfair+Display:wght@400;500;600;700" +
+  "&family=Merriweather:wght@300;400;700" +
+  "&family=Lora:wght@400;500;600;700" +
+  "&family=Fira+Code:wght@400;500;600;700" +
+  "&family=Source+Code+Pro:wght@400;500;600;700" +
+  "&family=JetBrains+Mono:wght@400;500;600;700" +
+  "&family=Pacifico" +
+  "&family=Dancing+Script:wght@400;500;600;700" +
+  "&display=swap";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => (
   <html lang='en'>
-    <body
-      className={`${workSans.className} ${fontVariables} bg-primary-grey-200`}
-    >
+    <head>
+      <link rel='preconnect' href='https://fonts.googleapis.com' />
+      <link
+        rel='preconnect'
+        href='https://fonts.gstatic.com'
+        crossOrigin='anonymous'
+      />
+      <link href={GOOGLE_FONTS_URL} rel='stylesheet' />
+    </head>
+    <body className='bg-primary-grey-200'>
       <TooltipProvider>{children}</TooltipProvider>
     </body>
   </html>
